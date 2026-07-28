62e33fce1173f928429dcc24964d5fe78d31381f
Harsh Dahat <harshdahat20@gmail.com>
2026-07-28 16:42:01 +0530

    Initial project setup

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("✅ Mongodb connected successfully");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ Mongodb connected successfully");
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = connectDB;
